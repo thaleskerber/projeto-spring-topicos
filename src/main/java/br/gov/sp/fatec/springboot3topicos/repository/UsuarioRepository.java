5838faf82fa0062f8f426c12c24f6a11d2bbc68a
thaleskerber <thaleskerber@gmail.com>
2024-04-08 15:42:16 -0300

    feat: add trabalho api

--- src/main/java/br/gov/sp/fatec/springboot3topicos/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.sp.fatec.springboot3topicos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.gov.sp.fatec.springboot3topicos.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
-}
