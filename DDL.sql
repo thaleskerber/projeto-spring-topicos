038f411994500fe76b4e54f423126e7f48fef033
Thales Kerber <thaleskerber@gmail.com>
2024-06-07 21:40:10 -0300

    Add prova02 entity

--- DDL.sql
+++ DDL.sql
@@ -11,5 +11,15 @@ cor_distancia_estrela float not null);
 
 insert into cor_corpo(cor_nome, cor_descricao, cor_raio, cor_distancia_estrela) values ('Terra', 'terceiro planeta', 6371, 1), ('Marte', 'quarto planeta', null, 1.5);
 
+create table bal_balanco (
+  bal_id bigint unsigned not null auto_increment,
+  bal_desc varchar(256) not null,
+  bal_data_hora datetime not null,
+  bal_valor numeric(10,2) not null,
+  primary key (bal_id)
+);
 
-select * from cor_corpo;
+insert into bal_balanco(bal_desc, bal_data_hora, bal_valor)
+  values('Supermercado', '2024-03-16 10:25', -221.45);
+insert into bal_balanco(bal_desc, bal_data_hora, bal_valor)
+  values('Salário', '2024-03-18 07:00', 4230.70);
